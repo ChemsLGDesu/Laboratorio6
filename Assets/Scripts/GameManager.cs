37cea1c0f68fe5de9a8e893f59d560c9f683a3bd
ChemsLGDesu <jamesgonelozano@gmail.com>
2025-10-06 18:39:45 -0500

    s

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,27 +2,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Player jugadorManager;
-    [SerializeField] private GuardTheAhsarah guardManager;
-    [SerializeField] private Saeed saeedManager;
-
+    [SerializeField] private Player Stinger;
+    [SerializeField] private GuardTheAhsarah guard;
+    [SerializeField] private Saeed saeed;
+    [SerializeField] private kit botiquin;
+    [SerializeField] private BuffItem buffDamage;
     void Start()
     {
-        Player Stinger = new Player();
-        GuardTheAhsarah guard = new GuardTheAhsarah();
-        Saeed saeed = new Saeed();
-        kit botiquin = new kit();
-        BuffItem buffItem = new BuffItem();
-
-        Stinger.MostrarInfo();
-        guard.MostrarInfo();
-        saeed.MostrarInfo();
+        Stinger = new GameObject("Player").AddComponent<Player>();
+        guard = new GameObject("Goblin").AddComponent<GuardTheAhsarah>();
+        saeed = new GameObject("Demonio").AddComponent<Saeed>();
+        botiquin = new GameObject("Pocion").AddComponent<kit>();
+        buffDamage = new GameObject("BuffItem").AddComponent<BuffItem>();
 
         Stinger.Atacar(guard);
         guard.Atacar(Stinger);
         saeed.Atacar(Stinger);
 
         botiquin.Usar();
-        buffItem.AplicarBuff(Stinger);
+        buffDamage.AplicarBuff(Stinger);
     }
 }
