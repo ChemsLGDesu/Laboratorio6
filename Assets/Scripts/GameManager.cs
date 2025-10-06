2acdf80c483c9aa0091b7f10912680795bef8acb
ChemsLGDesu <jamesgonelozano@gmail.com>
2025-10-06 16:05:53 -0500

    s

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,26 +2,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Player stinger;
-    public GuardTheAhsarah guardTheAhsarah;
-    public Saeed saeed;
+    public GameObject playerPrefab;
+    public GameObject guardPrefab;
+    public GameObject saeedPrefab;
     public GameObject kitPrefab;
+
+    private Player stinger;
+    private GuardTheAhsarah guardTheAhsarah;
+    private Saeed saeed;
 
     void Start()
     {
-        GameObject playerGO = new GameObject("Player");
-        stinger = playerGO.AddComponent<Player>();
+        // Instanciamos los personajes visibles en escena
+        GameObject playerGO = Instantiate(playerPrefab, new Vector3(-2, 0, 0), Quaternion.identity);
+        stinger = playerGO.GetComponent<Player>();
 
-        GameObject guardGO = new GameObject("GuardTheAhsarah");
-        guardTheAhsarah = guardGO.AddComponent<GuardTheAhsarah>();
+        GameObject guardGO = Instantiate(guardPrefab, new Vector3(2, 0, 0), Quaternion.identity);
+        guardTheAhsarah = guardGO.GetComponent<GuardTheAhsarah>();
 
-        GameObject saeedGO = new GameObject("Saeed");
-        saeed = saeedGO.AddComponent<Saeed>();
+        GameObject saeedGO = Instantiate(saeedPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        saeed = saeedGO.GetComponent<Saeed>();
 
+        // Spawneamos el kit
         Vector3 kitSpawn = new Vector3(0, 1, 0);
         GameObject go = Instantiate(kitPrefab, kitSpawn, Quaternion.identity);
-        kit kitComp = go.GetComponent<kit>();
-        kitComp.Usar(stinger);
 
         BuffItem buffColmena = new BuffItem();
 
