026045c8d4749ec6e504515a715356f6cdfb1e5a
ChemsLGDesu <jamesgonelozano@gmail.com>
2025-10-07 11:25:51 -0500

    s

--- Assets/Scripts/kit.cs
+++ Assets/Scripts/kit.cs
@@ -12,16 +12,15 @@ public class kit : Item, IConsumible
         if (Input.GetKeyDown(KeyCode.E))
         {          
             Usar();
-            lifeRecovered++;
-        }
-            
+        }           
     }
     public override void Usar()
     {
         Consumir();
     }
     public void Consumir()
-    { 
-        Debug.Log($"Usaste el {Name} y recuperaste {lifeRecovered} puntos de vida");  
+    {      
+        Debug.Log($"Usaste el {Name} y recuperaste {lifeRecovered} puntos de vida");
+        lifeRecovered++;
     }
 }
