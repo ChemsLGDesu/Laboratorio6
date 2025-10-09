ea05f680b5effa15fcfcdc30a89df9a5bc10e88c
ChemsLGDesu <jamesgonelozano@gmail.com>
2025-10-08 19:48:05 -0500

    s

--- Assets/Scripts/kit.cs
+++ Assets/Scripts/kit.cs
@@ -3,23 +3,37 @@ using UnityEngine;
 public class kit : Item, IConsumible
 {
     public int lifeRecovered = 50;
+    private Player player; // referencia al jugador
+
     private void Awake()
     {
         Name = "Botiquin de Campo";
+        player = FindObjectOfType<Player>(); // busca al Player en la escena
     }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
-        {          
+        {
             Usar();
-        }           
+        }
     }
+
     public override void Usar()
     {
         Consumir();
     }
+
     public void Consumir()
     {
-        Debug.Log($"Usaste el {Name} y recuperaste {lifeRecovered} puntos de vida");
+        if (player != null)
+        {
+            player.Vida += lifeRecovered; // aumenta la vida
+            Debug.Log($"Usaste el {Name} y recuperaste {lifeRecovered} puntos de vida. Vida actual: {player.Vida}");
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró al jugador para curar.");
+        }
     }
 }
